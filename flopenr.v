871d79926f37aa2c4cf9e469e54c7c98fabe1c09
StephenLe31415 <huy.lvsong@gmail.com>
2025-12-04 10:45:44 -0500

    change rst --> reset

--- flopenr.v
+++ flopenr.v
@@ -1,12 +1,12 @@
 module flopenr #(parameter W = 8)(
     input clk, 
-    input rst,
+    input reset,
     input en,
     input [W-1:0]d,
     output reg [W-1:0]q
 );
     
-    always @ (posedge clk, reset) begin
+    always @ (posedge clk or posedge reset) begin
         if (reset == 1'b1) q <= 0;
         else if (en == 1'b1) q <= d;
     end
