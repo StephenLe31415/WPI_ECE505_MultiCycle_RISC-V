f4af21da1d2c6eda7d8479816765091d8239d3c4
StephenLe31415 <huy.lvsong@gmail.com>
2025-12-04 10:46:57 -0500

    change rst --> reset

--- flopr.v
+++ flopr.v
@@ -1,12 +1,12 @@
 module flopr #(parameter W = 8) (
     input clk, 
-    input rst,
+    input reset,
     input [W-1:0] d,
     output reg [W-1:0] q
 );
     
     always @ (posedge clk or posedge reset) begin
-        if (rst == 1'b1) q <= 0;                  // reset: PC = 0x00000000
+        if (reset == 1'b1) q <= 0;                  // reset: PC = 0x00000000
         else q <= d;
     end
     
